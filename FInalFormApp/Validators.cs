04cbb159c098f45849231252ff57e901143e9265
NateIF22 <nathanfisnothere@gmail.com>
2026-03-14 17:43:02 -0700

    Add validators class

---
+++ FInalFormApp/Validators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalFormApp
+{
+    internal class Validators
+    {
+        public bool ValidateDeck(Deck deck)
+        {
+            if (string.IsNullOrWhiteSpace(deck.Name))
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(deck.Category))
+            {
+                return false;
+            }
+            if (deck.Cards == null || deck.Cards.Count == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
